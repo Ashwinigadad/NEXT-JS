ca8196bdd953c3a7ca03cd14318cda41f6fd704d
Ashwinigadad <ashwinigadad3@gmail.com>
2024-12-28 19:24:40 +0530

    loading

--- demo/app/product-order/page.tsx
+++ demo/app/product-order/page.tsx
@@ -5,10 +5,10 @@ export default function OrderProduct(){
     const handleClick=()=>{
         console.log("Placing your order");
         alert("Placing order");
-        router.push("/");
+        router.push("/register");
     }
     return (<>
     <h1>Order Product</h1>
-    <button className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-50" onClick={handleClick}>Place order</button>
+    <button className="bg-red-500 text-white font-bold py-2 px-4 rounded-full hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-50" onClick={handleClick}>Place order</button>
     </>)
 }
