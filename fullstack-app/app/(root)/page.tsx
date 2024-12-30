2bc836292a0febbeb98e6fd4f1932c64e87a9def
Ashwinigadad <ashwinigadad3@gmail.com>
2024-12-30 11:31:53 +0530

    fonts added and modified gobal css

--- fullstack-app/app/(root)/page.tsx
+++ fullstack-app/app/(root)/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-center text-3xl text-purple-600">Welcome Boss!</div>
-   
+   <>
+    <h1 className="heading">Home</h1>
+   </>
+
   );
 }
--- fullstack-app/app/components/Navbar.tsx
+++ fullstack-app/app/components/Navbar.tsx
@@ -1,51 +1,61 @@
-import { auth, signIn, signOut } from "@/auth"
-import Image from "next/image"
-import Link from "next/link"
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
 
-const Navbar = async()=>{
-  const session = await auth()
-  
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-        <nav className="flex justify-between items-center">
-          <Link href="/">
-            <Image src="/LOGONEXT.png" alt="logo" width={40} height={10} />
+    <header className="px-5 py-2 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <div className="flex items-center">
+          <Link href="/" className="flex items-center">
+            <Image src="/LOGONEXT.png" alt="logo" width={40} height={40} />
+            <span className="text-black font-bold text-2xl ml-0">Directory</span>
           </Link>
-          
-          <div className="flex items-center gap-5 text-black">
-              {session && session?.user ? (
-                <>
-                  <>
-                    <Link href="/startup/create">
-                      <span> 
-                        Create
-                      </span>
-                    </Link>
-                <form action={async() => { 
-                  "use server"
-                       await signOut()
-                }}>
-                  <button type="submit">Logourt</button>
-                    </form>
-                    
-                    <Link href={`/user/${session?.id}`}>
-                  <span>{session?.user?.name}</span>
-                    </Link>
-                  </>
-                </>
-              ):(
-              <form action={async()=>{
-                "use server"
-                await signIn("github")
-              }}>
-                  <button type="submit">Login</button>
-                </form>
-              )
-              }
-          </div>
-        </nav>
-      </header>
-  )
-}
+        </div>
+        <div className="flex items-center gap-4 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span className="font-semibold">Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button
+                  type="submit"
+                  className="font-semibold text-red-400 py-2 px-4 rounded"
+                >
+                  Logout
+                </button>
+              </form>
+              <Link href={`/user/${session?.id}`}>
+                <span className="font-semibold">{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button
+                type="submit"
+                className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full"
+              >
+                Login
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
