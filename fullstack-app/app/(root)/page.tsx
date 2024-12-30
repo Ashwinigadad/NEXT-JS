c7ee916480fed491cceba2da024de8b9b5370151
Ashwinigadad <ashwinigadad3@gmail.com>
2024-12-30 10:13:19 +0530

    Navbar

--- fullstack-app/app/(root)/page.tsx
+++ fullstack-app/app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Welcome Boss!</div>
+    <div className="text-center text-3xl text-purple-600">Welcome Boss!</div>
    
   );
 }
