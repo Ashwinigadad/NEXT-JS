c3c5ceac80e95402f35835a0cf7b0f61bdcacc01
Ashwinigadad <ashwinigadad3@gmail.com>
2024-12-30 18:35:09 +0530

    hero section with search form

--- fullstack-app/app/(root)/page.tsx
+++ fullstack-app/app/(root)/page.tsx
@@ -1,5 +1,9 @@
-import SearchForm from "../components/SearchForm";
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+export default async function Home({searchParams}:{
+  searchParams:Promise<{query?:string}>
+}) {
+  const query = (await searchParams).query;
+
   return (
    <>
     <section className="orange_container">
@@ -8,7 +12,7 @@ export default function Home() {
       <p className="sub-heading !max-w-3xl">
         Submit Ideas,Vote on Pitches, and Get Noticed in Virtual Competitions.
       </p>
-      <SearchForm />
+      <SearchForm query={query}/>
    </section>
    </>
 
--- fullstack-app/components/SearchForm.tsx
+++ fullstack-app/components/SearchForm.tsx
@@ -1,20 +1,20 @@
 import Form from "next/form"
 import SearchFormReset from "./SearchFormReset"
-function SearchForm() {
-  const query = 'Test'
+import { Search } from "lucide-react"
+function SearchForm({query} : {query?:string}) {
     
   return (
     <Form action="/" scroll={false} className="search-form">
         <input
             name="query"
-            defaultValue="Test"
+            defaultValue={query}
             className="search-input"
             placeholder="Search Startups"
         />
         <div className="flex gap-2">
             {query && <SearchFormReset/>}
 
-            <button type="submit" className="search-btn text-white">S</button>
+            <button type="submit" className="search-btn text-white"><Search/></button>
         </div>
 
     </Form>
