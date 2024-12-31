f6ebed7d1a9d77ead145a0357377c01a12f4c7a7
Ashwinigadad <ashwinigadad3@gmail.com>
2024-12-31 10:57:51 +0530

    designed startup section

--- fullstack-app/app/(root)/page.tsx
+++ fullstack-app/app/(root)/page.tsx
@@ -1,8 +1,19 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 export default async function Home({searchParams}:{
   searchParams:Promise<{query?:string}>
 }) {
   const query = (await searchParams).query;
+  const posts = [{
+    _createdAt:new Date(),
+    views:55,
+    author:{_id:1,name:"Ash"},
+    _id:1,
+    description:"description",
+    image:"https://wallpapers.com/images/featured/luffy-pictures-22osajj9ivjfcm49.webp",
+    category:"Robots",
+    title:"We Robots"
+  }]
 
   return (
    <>
@@ -10,9 +21,23 @@ export default async function Home({searchParams}:{
       <h1 className="heading">Pitch Your startup, <br />Connect with Startup Leaders</h1>
 
       <p className="sub-heading !max-w-3xl">
-        Submit Ideas,Vote on Pitches, and Get Noticed in Virtual Competitions.
-      </p>
+      Share your ideas, cast your votes on pitches, and shine in virtual competitions.      </p>
       <SearchForm query={query}/>
+   </section>
+
+   <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `Search results for "${query}"`: 'All Startups'}
+      </p>
+      <ul className="mt-7 card_grid">
+        {posts?.length > 0 ?(posts.map((post:StartupCardType,index:number)=>(
+          <StartupCard key={post?._id} post={post}/>
+        ))
+      ):(
+        <p className="no-results">Search not found</p>
+      )}
+
+      </ul>
    </section>
    </>
 
