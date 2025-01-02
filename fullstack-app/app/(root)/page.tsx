6975a00548d626831e20a8dc384fd9099c4233ae
Ashwinigadad <ashwinigadad3@gmail.com>
2025-01-02 10:06:43 +0530

    fetching real time data from sanity

--- fullstack-app/app/(root)/page.tsx
+++ fullstack-app/app/(root)/page.tsx
@@ -1,19 +1,24 @@
 import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 export default async function Home({searchParams}:{
   searchParams:Promise<{query?:string}>
 }) {
   const query = (await searchParams).query;
-  const posts = [{
-    _createdAt:new Date(),
-    views:55,
-    author:{_id:1,name:"Ash"},
-    _id:1,
-    description:"description",
-    image:"https://wallpapers.com/images/featured/luffy-pictures-22osajj9ivjfcm49.webp",
-    category:"Robots",
-    title:"We Robots"
-  }]
+
+  const posts=await client.fetch(STARTUPS_QUERY);
+  console.log(JSON.stringify(posts,null,2))
+  // const posts = [{
+  //   _createdAt:new Date(),
+  //   views:55,
+  //   author:{_id:1,name:"Ash"},
+  //   _id:1,
+  //   description:"description",
+  //   image:"https://wallpapers.com/images/featured/luffy-pictures-22osajj9ivjfcm49.webp",
+  //   category:"Robots",
+  //   title:"We Robots"
+  // }]
 
   return (
    <>
